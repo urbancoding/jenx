ed71eae894356497b1923a230395bbff0d5b7965
Trent Kocurek <t2kocurek@gmail.com>
2011-05-18 20:29:41 -0500

    load all projects into menu on load

--- jenx/AppDelegate.rb
+++ jenx/AppDelegate.rb
@@ -5,8 +5,14 @@
 #  Created by Trent Kocurek on 5/18/11.
 #  Copyright 2011 Urban Coding. All rights reserved.
 #
+require 'rubygems'
+require 'json'
+require 'open-uri'
 
 class AppDelegate
+    JENKINS_URL = '' #enter your url here
+    API_URL_EXTENSION = 'api/json'
+    
     attr_accessor :menu
     
     def awakeFromNib
@@ -16,11 +22,27 @@ class AppDelegate
         @status_item.setMenu(@menu)
         
         @app_icon = NSImage.imageNamed('app.tif')
+        @build_failure_icon = NSImage.imageNamed('build_failure.tif')
         
         @status_item.setImage(@app_icon)
+        @status_item.menu.insertItem(NSMenuItem.separatorItem, atIndex:1)
+        
+        load_jobs
     end
     
     def get_build_status(sender)
+    end
+    
+    def load_jobs
+        results = JSON.parse(open(JENKINS_URL + API_URL_EXTENSION).string)
+        
+        results['jobs'].each_with_index do |job, index|
+            builds_menu_item = NSMenuItem.alloc.init
+            builds_menu_item.title = " " + job['name']
+            builds_menu_item.enabled = true
+            builds_menu_item.setImage(NSImage.imageNamed('app.tif'))
+            @status_item.menu.insertItem(builds_menu_item, atIndex:index + 2)
+        end
     end
 end
 
