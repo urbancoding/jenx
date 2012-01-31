6349db196abc374cec3f3c7aac09a019dbc52ec9
Marc Abramowitz <marc@marc-abramowitz.com>
2012-01-31 13:15:03 -0800

    Fix GH-38

--- tests/run_suite.rb
+++ tests/run_suite.rb
@@ -15,14 +15,11 @@ FakeWeb.allow_net_connect = false
 
 # Loading all the Ruby project files.
 dir_path = File.expand_path('../../jenx/', __FILE__)
-puts dir_path
-Dir.glob(File.join(dir_path, '**/*.{rb,rbo}')).map { |x| File.basename(x, File.extname(x)) }.uniq.each do |path|
-    p = dir_path + '/' + path
-    require(p) unless path == 'rb_main'
+['JenxLocalize', 'JenxConstants', 'JenxConnection', 'JenxConnectionManager'].each do |class_name|
+	require "#{dir_path}/#{class_name}.rb"
 end
 
 require File.expand_path('../JenxTestCase.rb',  __FILE__)
-Dir.glob(File.expand_path('../**/*Test.rb', __FILE__)).each do |test| 
+Dir.glob(File.expand_path('../**/*Test.rb', __FILE__)).each do |test|
     require test 
 end
-
